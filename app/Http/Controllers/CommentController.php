7d001fc08004ccdcc5fa32e656ebed5df7d42240
Andonigt04 <andonigt04@gmail.com>
2026-04-24 11:06:45 +0200

    Implement CRUD methods in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,5 +7,33 @@ use Illuminate\Routing\Controller;
 
 class CommentController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store a new post
+    }
+
+    public function show($id)
+    {
+        // Logic to show a specific post
+    }
+
+    public function edit($id)
+    {
+        // Logic to edit a specific post
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update a specific post
+    }
+
+    public function destroy($id)
+    {
+        // Logic to delete a specific post
+    }
 }
